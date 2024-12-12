27e0636be6d101d0e3d1ecfee82a0d5ae72d0bd2
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2024-12-12 12:32:44 -0500

    dsfdsf

---
+++ app/JS/items.js
@@ -0,0 +1,358 @@
+export const loveinterest =
+[
+    {
+        name: "Torrent",
+        affectionlvl: 100,
+        img: "",
+        maplocation: "Always with you"
+    },
+    {
+        name: "Melina",
+        affectionlvl: 50,
+        img: "",
+        maplocation: "The Table of Lost Grace"
+    },
+    {
+        name: "Blaidd",
+        affectionlvl: 60,
+        img: "",
+        maplocation: "Siofra River, Mistwood Ruins"
+    },
+    {
+        name: "Ranni the Witch",
+        affectionlvl: 50,
+        img: "",
+        maplocation: "Three Sisters, Caria Manor"
+    },
+    {
+        name: "Patches",
+        affectionlvl: 10,
+        img: "",
+        maplocation: "Murkwater Cave, Roundtable Hold"
+    },
+    {
+        name: "Varré",
+        affectionlvl: 50,
+        img: "",
+        maplocation: "Limgrave, Roundtable Hold"
+    },
+    {
+        name: "Fia",
+        affectionlvl: 50,
+        img: "",
+        maplocation: "Roundtable Hold, Deeproot Depths"
+    },
+    {
+        name: "Godrick the Grafted",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Stormveil Castle"
+    },
+    {
+        name: "Radahn, Starscourge",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Caelid, Redmane Castle"
+    },
+    {
+        name: "Rykard, Lord of Blasphemy",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Volcano Manor"
+    },
+    {
+        name: "Malenia, Blade of Miquella",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Elphael, Brace of the Haligtree"
+    },
+    {
+        name: "Miquella",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Haligtree, in his dream form"
+    },
+    {
+        name: "Mohg, Lord of Blood",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Mohgwyn Palace"
+    },
+    {
+        name: "Commander Niall",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Castle Sol"
+    },
+    {
+        name: "Sir Gideon Ofnir",
+        affectionlvl: 20,
+        img: "",
+        maplocation: "Roundtable Hold"
+    },
+    {
+        name: "Ensha of the Royal Remains",
+        affectionlvl: 30,
+        img: "",
+        maplocation: "Roundtable Hold"
+    },
+    {
+        name: "Nepheli Loux",
+        affectionlvl: 50,
+        img: "",
+        maplocation: "Stormveil Castle"
+    },
+    {
+        name: "Hyetta",
+        affectionlvl: 50,
+        img: "",
+        maplocation: "Lake of Liurnia"
+    },
+    {
+        name: "D, Hunter of the Dead",
+        affectionlvl: 45,
+        img: "",
+        maplocation: "Roundtable Hold, Altus Plateau"
+    },
+    {
+        name: "Gurranq, Beast Roar",
+        affectionlvl: 30,
+        img: "",
+        maplocation: "Bestial Sanctum, Caelid"
+    },
+    {
+        name: "Roderika",
+        affectionlvl: 50,
+        img: "",
+        maplocation: "Stormveil Castle, Roundtable Hold"
+    },
+    {
+        name: "Tanith",
+        affectionlvl: 50,
+        img: "",
+        maplocation: "Volcano Manor"
+    },
+    {
+        name: "Blackguard Big Boggart",
+        affectionlvl: 20,
+        img: "",
+        maplocation: "Liurnia of the Lakes"
+    },
+    {
+        name: "Gideon the All-Knowing",
+        affectionlvl: 10,
+        img: "",
+        maplocation: "Roundtable Hold"
+    },
+    {
+        name: "Hoarah Loux",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Leyndell, Royal Capital"
+    },
+    {
+        name: "Giant Godskin Apostle",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Forbidden Lands"
+    },
+    {
+        name: "The Three Fingers",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Royal Capital Sewers"
+    },
+    {
+        name: "Shabiri",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Subterranean Shunning-Grounds"
+    },
+    {
+        name: "Elden Beast",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Erdtree"
+    },
+    {
+        name: "Radagon of the Golden Order",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Erdtree"
+    },
+    {
+        name: "Iron Fist Alexander",
+        affectionlvl: 55,
+        img: "",
+        maplocation: "Caelid, Redmane Castle"
+    },
+    {
+        name: "Gloam-Eyed Queen",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Forbidden Lands"
+    },
+    {
+        name: "Morgott, The Omen King",
+        affectionlvl: -50,
+        img: "",
+        maplocation: "Leyndell, Royal Capital"
+    },
+    {
+        name: "Rennala, Queen of The Full Moon",
+        affectionlvl: 20,
+        img: "",
+        maplocation: "Academy of Raya Lucaria "
+    },
+    {
+        name: "Royal Knight Loretta",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Caria Manor"
+    },
+    {
+        name: "Maliketh, The Black Blade",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Crumbling Farum Azula"
+    },
+    {
+        name: "Dragonlord Placidusax",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Crumbling Farum Azula"
+    },
+    {
+        name: "Fire Giant",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Mountaintops of The Giants"
+    },
+    {
+        name: "Lichdragon Fortissax",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Deeproot Depths"
+    },
+    {
+        name: "Draconic Tree Sentinel",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Leyndell, Royal Capital"
+    },
+    {
+        name: "Tree Sentinel",
+        affectionlvl: -20,
+        img: "",
+        maplocation: "Limgrave"
+    },
+    {
+        name: "Astel, Naturalborn of The Void",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Lake of Rot"
+    },
+    {
+        name: "Sorceress Sellen",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Waypoint Ruins"
+    },
+    {
+        name: "Irina",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Weeping Penisula"
+    },
+    {
+        name: "Rya",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Volcano Manor"
+    },
+    {
+        name: "Enia",
+        affectionlvl: 30,
+        img: "",
+        maplocation: "Roundtable Hold"
+    },
+    {
+        name: "Millicent",
+        affectionlvl: 50,
+        img: "",
+        maplocation: "Church of The Plague"
+    },
+    {
+        name: "Miriel, Pastor of Vows",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Church of Vows"
+    },
+    {
+        name: "Boc",
+        affectionlvl: 40,
+        img: "",
+        maplocation: "Limgrave"
+    },
+    {
+        name: "Godwyn, Prince of Death",
+        affectionlvl: 100,
+        img: "",
+        maplocation: "Deeproot Depths"
+    },
+    {
+        name: "Adan, The Thief Fire",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "The Malefactor's Evergaol"
+    },
+    {
+        name: "Bols, Carian Knight",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Cuckoo's Evergaols"
+    },
+    {
+        name: "Seluvis",
+        affectionlvl: 30,
+        img: "",
+        maplocation: "Seluvis's Rise"
+    },
+    {
+        name: "Merchant Kalé",
+        affectionlvl: 30,
+        img: "",
+        maplocation: "Church of Elleh"
+    },
+    {
+        name: "Smithing Master Hewg",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Roundtable Hold"
+    },
+    {
+        name: "Thops",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Church of Irith"
+    },
+    {
+        name: "Edgar",
+        affectionlvl: -10,
+        img: "",
+        maplocation: "Castle Morne"
+    },
+    {
+        name: "Rogier",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Stormveil Castle"
+    },
+    {
+        name: "War Counselor Iji",
+        affectionlvl: 0,
+        img: "",
+        maplocation: "Kingsrealm Ruins"
+    },
+
+]
