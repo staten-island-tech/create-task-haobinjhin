07c9e45e5b60c22ef16408626239a5afc14f48a1
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2024-12-16 12:30:27 -0500

    dfsdfsdfs

--- app/JS/main.js
+++ app/JS/main.js
@@ -17,15 +17,20 @@ function characterroute(name, img, route, affectionlvl){
         }
     ]
 
-    return console.log(character)
+    return character
 }
 
 function makecard(character){
-    for (let i = 0; i<= loveinterests.length; i++){
+    for (let i = 0; i<= character.length - 1; i++){
         dom.cardbox.insertAdjacentHTML("beforeend",
-            ""
+        `<div class = "card"> 
+        <h1 class = "name">${character[i].name}</h1>
+        <h5 class = "affectionlvl"> ${character[i].affectionlvl}</h5>
+        <img src="${character[i].img}" alt="${character[i].name}" class = "image">
+        </div>`
         )
-
-
     }
 }
+
+
+makecard(loveinterests)
