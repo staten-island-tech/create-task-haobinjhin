368541138c8b96f103417cb583870d6322b5b7a0
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2024-12-18 20:13:03 -0500

    sdfsdf

--- app/JS/main.js
+++ app/JS/main.js
@@ -3,38 +3,67 @@ import {loveinterests} from "./items";
 
 const dom = {
     cardbox: document.querySelector(".cardbox"),
-    card: document.querySelector(".card")
+    goback: document.querySelector(".back")
 
 }
 
-console.log(loveinterests)
+/* function characterroute(card){
+    
+    const name = card.querySelector(".name").innerText;
+    const affectionlvl = card.querySelector(".affectionlvl").innerText;
+    const gender = card.querySelector(".gender").innerText;
+    const maplocation = card.querySelector(".maplocation").innerText;
+    const img = card.querySelector(".image").src;
 
-function characterroute(name, img, route, affectionlvl){
     let character = [
         {name: name,
-         img: img,
-         route: route,
-         affection: affectionlvl
+        affectionlvl: affectionlvl,
+        img: img,
+        route: "",
+        gender: gender,
+        maplocation: maplocation,
         }
     ]
 
     return character
-}
+} */
+
 
 function makecard(character){
     for (let i = 0; i<= character.length - 1; i++){
         dom.cardbox.insertAdjacentHTML("beforeend",
         `
-        <button class = "card"> 
+        <button class = "card" id = "${character[i].name}"> 
         <h1 class = "name">${character[i].name}</h1>
         <h5 class = "affectionlvl"> ${character[i].affectionlvl}</h5>
+        <h5 class = "gender"> ${character[i].gender}</h5>
+        <h5 class = "maplocation"> ${character[i].maplocation}</h5>
         <img src="${character[i].img}" alt="${character[i].name}" class = "image">
         </button>`
         )
     }
 }
 
+
+
 makecard(loveinterests)
+const cards = document.querySelectorAll(".card")
 
 
-dom.card.addEventListener("click", ()=>console.log("hellow"))
+
+function chosenroute(interest){
+    let character = []
+    character.push(interest.querySelector(".name").innerHTML)
+    loveinterests.forEach((luv) => {
+        if (luv.name == character[0]){
+            dom.cardbox.innerHTML = " "
+            dom.cardbox.insertAdjacentElement("beforeend", interest)
+            
+        }
+    })
+}
+
+
+
+
+cards.forEach((card) => {card.addEventListener("click", function(){chosenroute(card)})})
